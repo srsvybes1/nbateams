63dcb07ac6b9e5e3767f05c0da288ec27a844d62
Cody Toney-Griffith <cody.toney@gmail.com>
2020-05-18 16:32:15 +0000

    added body

--- lib/nbateams/cli.rb
+++ lib/nbateams/cli.rb
@@ -1,7 +1,39 @@
 class Nbateams::CLI 
   
-  def call 
-    puts " this still the teams fam"
-  end 
+    list_teams
+    menu
+    cliexit 
+  end
+
+  def list_teams
+    puts "Your NBA Teams"
+    @teams = Nbateams::Teams.today
+    @teams.each.with_index(1) do |teams|
+      puts "#{teams.name} - #{teams.coach} - #{teams.salarycap}"
+    end
+  end
+
+  def menu
+    input = nil
+    while input != "exit"
+      puts "Enter Your Team"
+      input = gets.strip.downcase
+
+      if input = "Bulls"
+        the_team = @teams[input]
+        puts "#{the_team.name} - #{the_team.coach} - #{the_team.salarycap}"
+      elsif input == "list"
+        list_teams
+      else
+        puts "Error, type list or exit."
+      end
+    end
+  end
+
+  def exitcli
+    puts "See you on the Court!"
+  end
+end
+
   
 end 
