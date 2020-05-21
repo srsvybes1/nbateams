4ec51d2477b201bffccb6bdb36e433d7e431ed2c
Cody Toney-Griffith <cody.toney@gmail.com>
2020-05-21 02:12:26 +0000

    added noko

--- lib/nbateams/teams.rb
+++ lib/nbateams/teams.rb
@@ -1,5 +1,5 @@
 class Nbateams::Teams
-attr_accessor :name, :url, :coach, :players, :championships 
+attr_accessor :name, :url, :coach, :players, :championships, :record 
 
 def self.today 
 self.scrape_teams 
@@ -29,8 +29,11 @@ teams
 end 
 def self.scrape_bulls 
   doc = Nokogiri::HTML(open("https://www.landofbasketball.com/teams/chicago_bulls.htm"))
-  name = doc.search("h1.tc-1").text
-  coach = 
+  name = doc.search("div p")[3].text
+  coach = doc.search("td p")[4].text
+  websiteinfo = doc.search("div p")[2].text 
+  record = doc.search("div p")[6].text 
+  championships = doc.search("div p")[7].text 
   binding.pry
 end 
 end 
